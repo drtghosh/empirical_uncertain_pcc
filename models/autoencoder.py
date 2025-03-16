37b9861f93c92b106ffacdca7135b1dc4234b54c
Debabrata Ghosh <rijugh@gmail.com>
2025-03-16 15:48:39 +0100

    ae residual fix 2

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -47,7 +47,8 @@ class EncoderPC(nn.Module):
 		for idx, nf in enumerate(self.n_features):
 			if idx in self.residual_layers:
 				global_feature = torch.max(x, dim=2, keepdim=True)[0]
-				x = torch.cat([global_feature.expand(-1, partial_num, -1), x], dim=1)
+				_, _, feature_size = x.shape
+				x = torch.cat([global_feature.expand(-1, feature_size, -1), x], dim=1)
 			if self.normalize:
 				x = self.model[3*idx + 2](self.model[3*idx + 1](self.model[3*idx](x)))
 			else:
