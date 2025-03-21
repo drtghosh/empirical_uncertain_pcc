a3b840991d30156e31281b3637bd2db9482cccca
Debabrata Ghosh <rijugh@gmail.com>
2025-03-21 22:24:54 +0100

    negative sample validation pcn car

--- datasets/data_utils.py
+++ datasets/data_utils.py
@@ -244,4 +244,4 @@ id_dict = {
     "cabinet": "02933112",  # dresser
     "car": "02958343"
 }
-# save_negative_complete_pcn('data/PCN', 'train', 'car', id_dict, True)
+save_negative_complete_pcn('data/PCN', 'validation', 'car', id_dict, True)
