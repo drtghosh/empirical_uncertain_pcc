c7d1b9d5e76bdb795a88184d9e45c540e216f5dc
Debabrata Ghosh <rijugh@gmail.com>
2025-03-24 20:27:30 +0100

    contrastive script gp batch fixed

--- scripts/pcn/test-con-car.sh
+++ scripts/pcn/test-con-car.sh
@@ -6,5 +6,5 @@ python test_contrastive.py -l proj_log_con \
                           -c car \
                           -b 1 \
                           -a proj_log_ae/pcn_car_ae/ae/model/ckpt_epoch100.pth \
-                          --ckpt 100
+                          --ckpt 100 \
                           --gp_batch 100
