977ba7a88c7624207edd8b5418f79bbfd34fa106
Debabrata Ghosh <rijugh@gmail.com>
2025-03-24 16:05:03 +0100

    contrastive script fix

--- scripts/pcn/test-con-car.sh
+++ scripts/pcn/test-con-car.sh
@@ -1,9 +1,9 @@
-python test_imle.py -l proj_log_con \
-                    -x pcn_car_con \
-                    -m contrast_ae \
-                    -n pcn \
-                    -r data/PCN \
-                    -c car \
-                    -b 1 \
-                    -a proj_log_ae/pcn_car_ae/ae/model/ckpt_epoch100.pth \
-                    --ckpt 100
+python test_contrastive.py -l proj_log_con \
+                          -x pcn_car_con \
+                          -m contrast_ae \
+                          -n pcn \
+                          -r data/PCN \
+                          -c car \
+                          -b 1 \
+                          -a proj_log_ae/pcn_car_ae/ae/model/ckpt_epoch100.pth \
+                          --ckpt 100
