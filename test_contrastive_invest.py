377f8d0c6f7e910b2651198a12ac65a97ef1bc6f
Debabrata Ghosh <rijugh@gmail.com>
2025-04-01 17:19:53 +0200

    contrastive investigate

--- test_contrastive_invest.py
+++ test_contrastive_invest.py
@@ -14,7 +14,7 @@ from gpytoolbox import write_mesh, fd_interpolate
 from skimage.measure import marching_cubes
 
 
-def test_con_tr():
+def test_con():
     # create experiment config containing all hyperparameters
     config = get_config('test')
 
@@ -28,23 +28,23 @@ def test_con_tr():
     # create dataloader
     config.batch_size = 1
     config.num_workers = 1
-    test_train_loader = get_dataloader('train', config)
-    num_test = len(test_train_loader)
+    test_loader = get_dataloader('test', config)
+    num_test = len(test_loader)
     print(f"Total number of test samples: {num_test}.")
     saved_test = num_test if config.num_sample == -1 else config.num_sample
     print(f"Selected number of test samples to be saved: {saved_test}.")
-    test_train_loader = cycle(test_train_loader)
+    test_loader = cycle(test_loader)
 
     # directory to save results
     save_dir = os.path.join(config.proj_dir,
-                            "results_on_tr/ckpt-{}-n{}".format(config.ckpt, saved_test))
+                            "results/ckpt-{}-n{}".format(config.ckpt, saved_test))
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
     # test
     with torch.no_grad():
         for _ in tqdm(range(saved_test)):
-            data = next(test_train_loader)
+            data = next(test_loader)
             trainer.forward(data, False)
 
             pc_dir = os.path.join(save_dir, trainer.data_id[0])
@@ -112,4 +112,4 @@ def test_con_tr():
 
 
 if __name__ == '__main__':
-    test_con_tr()
+    test_con()
