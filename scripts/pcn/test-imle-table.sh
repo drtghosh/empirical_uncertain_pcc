0c03acd73cecc734b159c309b7a08e3543a93eae
Debabrata Ghosh <rijugh@gmail.com>
2025-04-06 00:03:17 +0200

    imle table test script

---
+++ scripts/pcn/test-imle-table.sh
@@ -0,0 +1,9 @@
+python test_imle.py -l proj_log_imle_ae_cl \
+                    -x pcn_table_imle_ae_cl \
+                    -m imle_gen \
+                    -n pcn \
+                    -r data/PCN \
+                    -c table \
+                    -b 1 \
+                    -a proj_log_ae_cl/pcn_table_ae_cl/ae/model/ckpt_epoch200.pth \
+                    --ckpt 200
