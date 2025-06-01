09e6ae3b127281d714cd04a7abee7d836c91b3a7
Debabrata Ghosh <rijugh@gmail.com>
2025-06-01 17:42:16 +0200

    hessian prediction check 12

--- test_hessian_lightning.py
+++ test_hessian_lightning.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 def test_hessian_simple():
     # create experiment config containing all hyperparameters
     config = get_config('test')
+    config.latent_dim = 256
 
     ckpt_path = os.path.join(config.model_dir, 'model.ckpt')
     ckpt = torch.load(ckpt_path)
