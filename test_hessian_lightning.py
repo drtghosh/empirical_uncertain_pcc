bbecd3291fee1001e0051e903a83b6444ec5a5a8
Debabrata Ghosh <rijugh@gmail.com>
2025-06-01 16:46:57 +0200

    hessian prediction check 6

--- test_hessian_lightning.py
+++ test_hessian_lightning.py
@@ -1,4 +1,5 @@
 import torch
+import pytorch_lightning as pl
 
 from configs import get_config
 from tools import get_trainer
@@ -11,14 +12,19 @@ import os
 from tqdm import tqdm
 
 
+class INR(pl.LightningModule):
+
+    def __init__(self, config):
+        super().__init__()
+        self.model = get_model(config, "HessSimple")
+
+
 def test_hessian_simple():
     # create experiment config containing all hyperparameters
     config = get_config('test')
 
     ckpt_path = os.path.join(config.model_dir, 'model.ckpt')
-    ckpt_dict = torch.load(ckpt_path)
-    model = get_model(config, "HessSimple")
-    model._load_from_state_dict(ckpt_dict['state_dict'])
+    model = INR.load_from_checkpoint(ckpt_path)
     print(model)
 
 
