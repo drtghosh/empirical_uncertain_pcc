0cf279224be7c1600668df18354c0ecdc4ac95cd
Debabrata Ghosh <rijugh@gmail.com>
2025-06-01 21:32:46 +0200

    contrastive post only manifold but a twist in cov

--- test_contrastive_invest.py
+++ test_contrastive_invest.py
@@ -153,8 +153,8 @@ def test_con():
             additional_noise = config.noise_variance * torch.eye(test_embedding.size(0)).to(trainer.device)
             cov_with_noise = (cov_pp + additional_noise)
             cov_inv = torch.linalg.inv(cov_with_noise)
-            cov_space_with_noise = (cov_pp_space + additional_noise.cpu())
-            cov_inv_space = torch.linalg.inv(cov_space_with_noise)
+            # cov_space_with_noise = (cov_pp_space + additional_noise.cpu())
+            cov_inv_space = torch.linalg.inv((cov_pp_space + additional_noise.cpu()))
             assert grid_embedding.size(
                 0) % config.gp_batch == 0, 'Number of grid points required to be a multiple of batch size'
             num_batches = grid_embedding.size(0) // config.gp_batch
