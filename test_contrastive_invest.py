e021625818267b7ce51b7ddf014ab65c5cae5967
Debabrata Ghosh <rijugh@gmail.com>
2025-06-01 20:30:32 +0200

    contrastive post with colors 9

--- test_contrastive_invest.py
+++ test_contrastive_invest.py
@@ -179,6 +179,7 @@ def test_con():
             var_on_vertices = W2 @ grid_posterior_var.cpu().numpy()
             # save mesh into .obj file
             write_mesh(os.path.join(pc_dir, 'mean_shifted.obj'), vertices, faces)
+            print(var_on_vertices)
             write_ply(os.path.join(pc_dir, 'mean_shifted_with_color.ply'), vertices, faces, var_on_vertices)
 
             # without mean shifting
