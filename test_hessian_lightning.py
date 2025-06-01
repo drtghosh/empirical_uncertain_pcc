3b551eed4364ae2a80fad6810635cc072fa40c47
Debabrata Ghosh <rijugh@gmail.com>
2025-06-01 16:09:33 +0200

    hessian prediction check

---
+++ test_hessian_lightning.py
@@ -0,0 +1,22 @@
+import torch
+
+from configs import get_config
+from tools import get_trainer
+from datasets import get_dataloader
+from datasets.data_utils import cycle
+
+import os
+from tqdm import tqdm
+
+
+def test_hessian_simple():
+    # create experiment config containing all hyperparameters
+    config = get_config('test')
+
+    ckpt_path = os.path.join(config.model_dir, 'model.ckpt')
+    ckpt = torch.load(ckpt_path)
+    print(ckpt)
+
+
+if __name__ == '__main__':
+    test_hessian_simple()
