0bdb374fdd25b8ea08a63334121c2f17ec1e9690
Debabrata Ghosh <rijugh@gmail.com>
2025-06-02 08:59:52 +0200

    new interpolation 7

--- test_contrastive_invest.py
+++ test_contrastive_invest.py
@@ -75,6 +75,7 @@ def test_con():
             # Build a grid (dimension-agnostic)
             grid_vertices = np.meshgrid(
                 *[np.linspace(box_min[d], box_max[d], grid_sizes[d]) for d in range(config.space_dim)])
+            grid_vertices = np.stack(grid_vertices, axis=-1).reshape(-1, config.space_dim)
 
             # create a grid around partial data
             grid_data, grid_sizes, corner, spacing = create_grid(trainer.partial_pc.transpose(1, 2), config.grid_size,
@@ -204,7 +205,7 @@ def test_con():
                                                               level=0.0)
             # W2 = fd_interpolate(vertices, grid_sizes, spacing, corner)
             # var_on_vertices = W2 @ grid_posterior_var.cpu().numpy()
-            print(grid_vertices)
+            print(grid_vertices.shape)
             interp = RegularGridInterpolator(grid_vertices, grid_posterior_var.cpu().numpy().reshape(
                 (grid_sizes[0], grid_sizes[1], grid_sizes[2])))
             var_on_vertices = interp(vertices)
