2add32ac5c0163998b3c299752433c0225b80cde
Debabrata Ghosh <rijugh@gmail.com>
2025-06-02 08:33:58 +0200

    new interpolation 3

--- test_contrastive_invest.py
+++ test_contrastive_invest.py
@@ -191,7 +191,7 @@ def test_con():
                                                               level=0.0)
             # W2 = fd_interpolate(vertices, grid_sizes, spacing, corner)
             # var_on_vertices = W2 @ grid_posterior_var.cpu().numpy()
-            interp = RegularGridInterpolator(grid_data.cpu().numpy().reshape((grid_sizes[0], grid_sizes[1], grid_sizes[2], 3)),
+            interp = RegularGridInterpolator(grid_data[0].cpu().numpy().reshape((grid_sizes[0], grid_sizes[1], grid_sizes[2], 3)),
                                              grid_posterior_var.cpu().numpy().reshape((grid_sizes[0], grid_sizes[1], grid_sizes[2])))
             var_on_vertices = interp(vertices)
             print(var_on_vertices)
