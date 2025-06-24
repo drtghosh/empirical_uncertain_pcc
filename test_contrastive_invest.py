10382024748ca9de63f41ee7faabced5de754f55
Debabrata Ghosh <rijugh@gmail.com>
2025-06-24 09:52:12 +0200

    gp test interpolate fd

--- test_contrastive_invest.py
+++ test_contrastive_invest.py
@@ -203,11 +203,13 @@ def test_con():
             # marching cubes
             vertices, faces, normals, values = marching_cubes(np.reshape(shifted_mean, grid_sizes, order='F'),
                                                               level=0.0)
-            # W2 = fd_interpolate(vertices, grid_sizes, spacing, corner)
-            # var_on_vertices = W2 @ grid_posterior_var.cpu().numpy()
-            interp = RegularGridInterpolator((x, y, z), grid_posterior_var.cpu().numpy().reshape(
-                (grid_sizes[0], grid_sizes[1], grid_sizes[2])))
-            var_on_vertices = interp(vertices)
+
+            print(grid_posterior_var)
+            W2 = fd_interpolate(vertices, grid_sizes, spacing, corner)
+            var_on_vertices = W2 @ grid_posterior_var.cpu().numpy()
+            # interp = RegularGridInterpolator((x, y, z), grid_posterior_var.cpu().numpy().reshape(
+            #    (grid_sizes[0], grid_sizes[1], grid_sizes[2])))
+            # var_on_vertices = interp(vertices)
             print(var_on_vertices)
             # save mesh into .obj file
             write_mesh(os.path.join(pc_dir, 'mean_shifted.obj'), vertices, faces)
